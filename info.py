578dae3fff4ac3a464feca24a8090844e568e226
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-09-13 16:11:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '16013849'))
 API_HASH = environ.get('API_HASH', 'c8686adc1a0c7cd17f2201c40123ce91')
-BOT_TOKEN = environ.get('BOT_TOKEN', ":AAEnUFFBbHfa_szP8bjnSbwkwgy6sfOJRNA")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6942886220:AAEnUFFBbHfa_szP8bjnSbwkwgy6sfOJRNA")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -36,8 +36,8 @@ auth_channel = environ.get('AUTH_CHANNEL','-1001622914589')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002190844194')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002134871596')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001722166824')
+reqst_channel = environ.get('REQST_CHANNEL_ID', -1002235689432')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
